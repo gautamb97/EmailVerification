cba8a40f9f8c3100cf9b3a3e2d02178a05f56ffb
gautamb97 <www.gautam971997@gmail.com>
2021-02-18 20:50:43 +0530

    Added:Ensuring @ and email second part validating

--- Email.java
+++ Email.java
@@ -3,20 +3,32 @@ import java.util.regex.Pattern;
 
 public class Email {
 	static Scanner userInput = new Scanner(System.in);
-	public void emailVerification() {
+	public void emailVerificationPartOne() {
 		System.out.println("Enter your email id: ");
 		String input = userInput.next();
 		if(Pattern.matches("^[a-z]{3,}$", input)) 
 			System.out.println("Valid");
 		else {
 			System.out.println("Invalid input re-enter email id: ");
-			emailVerification();
+			emailVerificationPartOne();
+		}
+	}
+	//ensuring @ and validating part two
+	public void emailVerificationPartTwo() {
+		System.out.println("Enter your email id: ");
+		String input = userInput.next();
+		if(Pattern.matches("^[a-z]{3,}\\@[a-z]*$", input)) 
+			System.out.println("Valid");
+		else {
+			System.out.println("Invalid input re-enter email id: ");
+			emailVerificationPartTwo();
 		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome for Email Verification");
 		Email validation = new Email();
-		validation.emailVerification();
+		validation.emailVerificationPartOne();
+		validation.emailVerificationPartTwo();
 	}
 
 }
