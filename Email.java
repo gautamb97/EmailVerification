05a250d24c3c667ad1aab6ca62fd423481d2eaa2
gautamb97 <www.gautam971997@gmail.com>
2021-02-18 20:15:57 +0530

    Added:Email verification part one from where email starts

--- Email.java
+++ Email.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Email {
-
+	static Scanner userInput = new Scanner(System.in);
+	public void emailVerification() {
+		System.out.println("Enter your email id: ");
+		String input = userInput.next();
+		if(Pattern.matches("^[a-z]{3,}$", input)) 
+			System.out.println("Valid");
+		else {
+			System.out.println("Invalid input re-enter email id: ");
+			emailVerification();
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome for Email Verification");
-
+		Email validation = new Email();
+		validation.emailVerification();
 	}
 
 }
