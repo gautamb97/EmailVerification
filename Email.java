4c0b7c5b2b18ddfd2dfc77d2d8ef52009c2e7fd3
gautamb97 <www.gautam971997@gmail.com>
2021-02-18 21:46:45 +0530

    Added:Ensuring dot and validating third part

--- Email.java
+++ Email.java
@@ -24,11 +24,23 @@ public class Email {
 			emailVerificationPartTwo();
 		}
 	}
+	// Ensuring "." and validating third part like .com 
+	public void emailVerificationPartThree() {
+		System.out.println("Enter your email id: ");
+		String input = userInput.next();
+		if(Pattern.matches("^[a-z]{3,}\\@[a-z]*\\.[a-z]*$", input)) 
+			System.out.println("Valid");
+		else {
+			System.out.println("Invalid input re-enter email id: ");
+			emailVerificationPartThree();
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome for Email Verification");
 		Email validation = new Email();
 		validation.emailVerificationPartOne();
 		validation.emailVerificationPartTwo();
+		validation.emailVerificationPartThree();
 	}
 
 }
