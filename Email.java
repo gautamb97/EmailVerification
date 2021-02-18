752b8dd1d8b78d78af7ead223488f4224a0b65cb
gautamb97 <www.gautam971997@gmail.com>
2021-02-18 22:28:38 +0530

    Added:Top level domains at the last of email

--- Email.java
+++ Email.java
@@ -39,11 +39,22 @@ public class Email {
 	public void specialCharacterBetweenEmailFirstPart() {
 		System.out.println("Enter your email id: ");
 		String input = userInput.next();
-		if(Pattern.matches("^[a-z\\\\+\\\\-\\\\_\\\\.a-z]{3,}\\@[a-z]*\\.[a-z]*$", input)) 
+		if(Pattern.matches("^[a-z\\+\\-\\_\\.a-z]{3,}\\@[a-z]*\\.[a-z]*$", input)) 
 			System.out.println("Valid");
 		else {
 			System.out.println("Invalid input re-enter email id: ");
 			specialCharacterBetweenEmailFirstPart();
+		}
+	}
+	//Adding top level domains at the last of Email
+	public void addingTopLevelDomainInEmail() {
+		System.out.println("Enter your email id: ");
+		String input = userInput.next();
+		if(Pattern.matches("^[a-z\\+\\-\\_\\.a-z]{3,}\\@[a-z]*\\.[a-z]{1,3}\\.[a-z]{2}$", input)) 
+			System.out.println("Valid");
+		else {
+			System.out.println("Invalid input re-enter email id: ");
+			addingTopLevelDomainInEmail();
 		}
 	}
 	public static void main(String[] args) {
@@ -53,6 +64,7 @@ public class Email {
 		validation.emailVerificationPartTwo();
 		validation.emailVerificationPartThree();
 		validation.specialCharacterBetweenEmailFirstPart();
+		validation.addingTopLevelDomainInEmail();
 	}
 
 }
